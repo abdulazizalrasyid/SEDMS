5c0f628e47e1816b0915f7219fbff5af77f8b7cc
Benny E. Willyanto <benny.ew@gmail.com>
2017-03-16 16:58:42 +0700

    ignoring some files

--- build/temp/development/Mail/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require D:\webroot\SEDMS2\Mail\app.js
--- build/temp/production/Mail/sass/config.rb
+++
@@ -1,3 +0,0 @@
-require '..\..\..\..\..\ext\packages\ext-theme-base\sass\utils.rb'
-Compass.add_project_configuration('..\..\..\..\..\Mail\sass\config.rb')
-cache_path = 'D:\webroot\SEDMS2/build/.sass-cache'
